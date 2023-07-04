175ddfcf44270b651c9a82984bd541db9e1a3c75
gurudevck <83879482+gurudevck@users.noreply.github.com>
2023-07-04 13:40:04 +0530

    Update hillcipher.c

--- hillcipher.c
+++ hillcipher.c
@@ -1,49 +1,83 @@
 #include<stdio.h>
-#include<string.h>
+#include<math.h>
 
-int main(){
-    unsigned int a[3][3]={{6,24,1},{13,16,10},{20,17,15}};
-    unsigned int b[3][3]= {{8,5,10},{21,8,21},{21,12,8}};
-    
-    int i,j,t=0;
-    unsigned int c[20], d[20];
-    char msg[20];
-    
-    printf("enter plain text\n");
-    scanf("%s",msg);
-    
-    for(i=0;i<strlen(msg);i++)
-    {
-        c[i]=msg[i]-65;
-        printf("%d",c[i]);
+float encrypt[3][1], decrypt[3][1], mes[3][1], a[3][3], b[3][3], c[3][3];
+
+void encrypt();
+void decrypt();
+void generateKeyMessage();
+void inverse();
+
+void main(){
+getKeyMessage();
+encrypt();
+decrypt();
+}
+
+void encrypt(){
+int i,j,k;
+for(i=0;i<3;i++)
+    for (j=0;j<1;j++)
+        for(k=0;k<3;k++)
+            encrypt[i][j]+=a[i][k]*mes[k][j];
+print("Encrypted String is : ");
+for(i=0;i<3;i++)
+    printf("%c",(char)(fmod(encrypt[i][0],26)+97));
+}
+void decrypt(){
+inverse();
+int i,j,k;
+for(i=0;i<3;i++)
+    for (j=0;j<1;j++)
+        for(k=0;k<3;k++)
+            decrypt[i][j]+=b[i][k]*encrypt[k][j];
+print("Decrypted String is : ");
+for(i=0;i<3;i++)
+    printf("%c",(char)(fmod(decrypt[i][0],26)+97));
+}
+void getKeyMessage(){
+int i,j;
+char msg[3];
+print("Enter 3x3 matrix for the key:\n");
+for(i=0;i<3;i++)
+    for(j=0;j<3;j++){
+        scanf("%f",&a[i][j]);
+        c[i][j]=a[i][j];
     }
-    for(i=0;i<3;i++)
-    {
-        t=0;
-        for(j=0;j<3;j++)
-        {
-            t= t+(a[i][j]*c[j]);
-        }
-        d[i]=t%26;
+printf("Enter 3 letter string : ");
+scanf("%s",msg);
+for(i=0;i<3;i++)
+    mes[i][0]=msg[i]-97;
+}
+void inverse(){
+int i,j,k;
+float p,q;
+for(i=0;i<3;i++)
+    for(j=0;j<3;j++){
+        if(i==j)
+            b[i][j]=1;
+        else
+            b[i][j]=0;
     }
-    printf("\n encrypted cipher text\n");
-    for(i=0;i<3;i++)
-    {
-        printf("%c",d[i]+65);
-    }
-    for(i=0;i<3;i++)
-    {
-        t=0;
-        for(j=0;j<3;j++)
-        {
-            t=t+(b[i][j]*d[j]);
-        }
-        c[i]=t%26;
-    }
-    printf("\n decrypted cipher text:");
-    for(i=0;i<3;i++)
-    {
-        printf("%c",c[i]+65);
-    }
-    return 0;
+for(k=0;k<3;k++){
+for(i=0;i<3;i++){
+p=c[i][k];
+q=c[k][k];
+for(j=0;j<3;j++){
+if(i!=k){
+c[i][j]=c[i][j]*q-p*c[k][j];
+b[i][j]=b[i][j]*q-p*b[k][j];
+}
+}
+}
+}
+for(i=0;i<3;i++)
+    for(j=0;j<3;j++){
+        b[i][j]=b[i][j]/c[i][j];
+printf("\nInverse Matrix\n");
+for(i=0;i<3;i++){
+    for(j=0;j<3;j++)
+        scanf("%f",&a[i][j]);
+    printf("\n");
+}
 }
